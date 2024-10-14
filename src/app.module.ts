026d94e73ef0d479fb3589859279dab01bdad57d
주건정 <jugeonjeong@jugeonjeong-ui-MacBookAir.local>
2024-10-15 00:31:43 +0900

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserService } from './application/service/user.service';
 import { UserModule } from './infrastructure/module/user.module';
 import { UserController } from './interface/controller/user.controller';
@@ -8,6 +7,6 @@ import { UserController } from './interface/controller/user.controller';
 @Module({
   imports: [UserModule],
   controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  providers: [UserService],
 })
 export class AppModule {}
