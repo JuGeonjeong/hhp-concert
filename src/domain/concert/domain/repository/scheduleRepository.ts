d80217ea7f1698d0eebc5a79200b7dbe41b1ca4a
Ju Geonjeong <81910342+JuGeonjeong@users.noreply.github.com>
2024-10-25 05:16:06 +0900

    Revert "Fix - 4주차 피드백"

--- src/domain/concert/domain/repository/scheduleRepository.ts
+++
@@ -1,6 +0,0 @@
-import Schedule from '../entity/schedule.entity';
-
-export interface ScheduleRepository {
-  findOne(id: number): Promise<Schedule>;
-  findSchedules(id: number): Promise<Schedule[] | []>;
-}
