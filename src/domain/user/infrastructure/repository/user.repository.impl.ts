406f264443b97b74bb37221a46c2891cc56b3b77
JuGeonjeong <geun0j@gmail.com>
2024-12-23 15:01:33 +0900

    release/1.2.4: Fix: PR 테스트

--- src/domain/user/infrastructure/repository/user.repository.impl.ts
+++ src/domain/user/infrastructure/repository/user.repository.impl.ts
@@ -20,6 +20,7 @@ export class UserRepositoryImpl implements UserRepository {
     const userEntity = await this.manager.save(entity);
     return UserMapper.toDomain(userEntity);
   }
+
   /**
    * @interface
    * @see {UserRepository.findOne}
